5ac063036594705141f788ea82e6a6d8d3414cb5
wojtek717 <wojciechkonury@gmail.com>
2020-02-18 16:01:01 +0100

    rounded button and working ball

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var ballImageView: UIImageView!
+    @IBOutlet weak var askButton: UIButton!
+    
     let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
-
-
-
+    
+    override func viewDidLoad() {
+        askButton.layer.cornerRadius = 25.0
+    }
+    
+    @IBAction func buttonOnTap(_ sender: Any) {
+        ballImageView.image = ballArray[Int.random(in: 0...4)]
+    }
+    
 }
 
